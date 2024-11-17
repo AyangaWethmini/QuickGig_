e0ca502c78ea132b98e70168caef52d520289184
ThuminduSena <thumindusenaratna@gmail.com>
2024-11-17 15:15:07 +0530

    Fix: Homepage

--- app/views/home/home.view.php
+++ app/views/home/home.view.php
@@ -1,7 +1,16 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <link rel="stylesheet" href="<?=ROOT?>/assets/css/home/home.css">
+<?php include APPROOT . '/views/components/navbar.php'; ?>
 
-<nav class="navbar flex-row">
+<script>
+    // Include the toggleMenu function
+    function toggleMenu() {
+      const navLinks = document.querySelector('.nav-links');
+      navLinks.classList.toggle('active');
+    }
+</script>
+
+<!-- <nav class="navbar flex-row">
     <div class="flex-row  nav-left">
         <div class="logo">QuickGig</div>
         <ul class="nav-links  flex-row">
@@ -19,7 +28,7 @@
         <span></span>
         <span></span>
     </div>
-</nav>
+</nav> -->
 
 <div class="hero-section flex-row">
     <div class="job-search">
