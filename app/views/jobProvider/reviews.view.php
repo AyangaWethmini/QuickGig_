74ad779a38aa03a5e0081d6b90940a12b0f3ce6f
Ayanga Wethmini <wethminiayanga@gmail.com>
2024-11-21 22:07:49 +0530

    reviews view

--- app/views/jobProvider/reviews.view.php
+++ app/views/jobProvider/reviews.view.php
@@ -1,53 +1,124 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<link rel="stylesheet" href="<?=ROOT?>/assets/css/JobProvider/reviews.css">
+<link rel="stylesheet" href="<?=ROOT?>/assets/css/JobProvider/findEmp.css">
 <?php require APPROOT . '/views/components/navbar.php'; ?>
 
-<div class="wrapper flex-row">
-    <?php require APPROOT . '/views/jobProvider/jobProvider_sidebar.php'; ?>
-    <div class="main-content">
-        <div class="job-cards container flex-col">
-            <h2>Reviews</h2>
-            <hr style="width: 100%">
-            <div class="search-container">
-                <input 
-                    type="text" 
-                    class="search-bar" 
-                    placeholder=" Find Employees"
-                    aria-label="Search" 
-                    style="height: 20px; width: 700px">
-            </div>
-            <div class="job-card container">
-                <div class="job-card-left flex-row">
-                    <div class="profile flex-row">
-                        <!-- Updated image src for debugging -->
-                        <img 
-                            src="<?=ROOT?>/assets/images/profile.png" 
-                            alt="Profile Picture" 
-                            class="profile-pic" 
-                            onerror="this.src='<?=ROOT?>/assets/images/default-profile.png';">
-                        <div class="review-details">
-                            <p class="name">Noah Wick</p>
-                            <p class="date-time">2024/09/4 at 12.45 a.m.</p>
-                            <div class="rating">
-                                <span>
-                                    <i class="fa fa-star star-active mx-1"></i>
-                                    <i class="fa fa-star star-active mx-1"></i>
-                                    <i class="fa fa-star star-active mx-1"></i>
-                                    <i class="fa fa-star star-active mx-1"></i>
-                                    <i class="fa fa-star star-active mx-1"></i>
-                                </span>
-                            </div>
+      <div class="wrapper flex-row">
+      <?php require APPROOT . '/views/jobProvider/jobProvider_sidebar.php'; ?>
+      <div class="main-content">
+        
+      <div class="job-cards container flex-col">
+      <h2>Reviews</h2>
+      <hr style="width: 100%">
+      <div class="search-container">
+            <input type="text" 
+                class="search-bar" 
+                placeholder=" Find Reviews"
+                aria-label="Search" style="height: 20px; width : 700px">
+      </div>
+          <div class="job-card container">
+              <div class="job-card-left flex-row">
+                  <div class="pfp">
+                    <img src="<?=ROOT?>/assets/images/profile.png" alt="Profile Picture" class="profile-pic">
+                  </div>
+                
+                  <div class="job-details">
+                      <h2>Noah Wick</h2>
+                      <p>2024/10/29 at 3.30 p.m.</p>
+                      <div style="display:flex;flex-direction:column; gap:20px">
+                        <div class="rating">
+                            <span>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                            </span>
                         </div>
-                    </div>
-
-                    <div class="review-text">
-                        <p class="text">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio maxime soluta inventore quis. Vel quos fuga debitis dicta laborum fugit libero eveniet! Error, placeat consectetur incidunt maiores mollitia cumque totam perspiciatis! Corrupti atque, illum id vero nulla eius ut cupiditate? Distinctio, animi. Placeat, illo eius.
+                        
+                        <p class="review-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla distinctio id adipisci dicta facere tempora atque veniam! Rerum, minus expedita nobis magnam vel quibusdam natus!
                         </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                      </div>
+                  </div>
+              </div>
+              
+          </div>
+
+
+
+          <div class="job-card container">
+              <div class="job-card-left flex-row">
+                  <div class="pfp">
+                    <img src="<?=ROOT?>/assets/images/profile.png" alt="Profile Picture" class="profile-pic">
+                  </div>
+                
+                  <div class="job-details">
+                      <h2>Noah Wick</h2>
+                      <p>2024/10/29 at 3.30 p.m.</p>
+                      <div style="display:flex;flex-direction:column; gap:20px">
+                        <div class="rating">
+                            <span>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                            </span>
+                        </div>
+                        
+                        <p class="review-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla distinctio id adipisci dicta facere tempora atque veniam! Rerum, minus expedita nobis magnam vel quibusdam natus!
+                        </p>
+                      </div>
+                  </div>
+              </div>
+              
+          </div>
+
+
+          <div class="job-card container">
+              <div class="job-card-left flex-row">
+                  <div class="pfp">
+                    <img src="<?=ROOT?>/assets/images/profile.png" alt="Profile Picture" class="profile-pic">
+                  </div>
+                
+                  <div class="job-details">
+                      <h2>Noah Wick</h2>
+                      <p>2024/10/29 at 3.30 p.m.</p>
+                      <div style="display:flex;flex-direction:column; gap:20px">
+                        <div class="rating">
+                            <span>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                                <i class="fa fa-star star-active mx-1"></i>
+                            </span>
+                        </div>
+                        
+                        <p class="review-text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla distinctio id adipisci dicta facere tempora atque veniam! Rerum, minus expedita nobis magnam vel quibusdam natus!
+                        </p>
+                      </div>
+                  </div>
+              </div>
+              
+          </div>
+
+
+
 </div>
+
+    
+
+
+
+
+
+         
+
+    
+
+
+
