a888e9891fe81bd6eee72c9fcd9c72cbdbe5d809
JaithLomitha2002 <djaithlomitha2002@gmail.com>
2024-11-25 14:05:04 +0530

    Update org_reviews.view.php

--- app/views/organization/org_reviews.view.php
+++ app/views/organization/org_reviews.view.php
@@ -1,127 +1,122 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-
-<link rel="stylesheet" href="<?=ROOT?>/assets/css/JobProvider/findEmp.css">
 <?php require APPROOT . '/views/components/navbar.php'; ?>
 
-      <div class="wrapper flex-row">
-      <?php require APPROOT . '/views/jobProvider/organization_sidebar.php'; ?>
-      <div class="main-content">
-        
-      <div class="job-cards container flex-col">
-      <h1>Reviews</h1>
-      <hr style="width: 100%">
-      <div class="search-container">
+<link rel="stylesheet" href="<?=ROOT?>/assets/css/JobProvider/reviews.css">
+
+<div class="wrapper flex-row">
+    <?php require APPROOT . '/views/jobProvider/organization_sidebar.php'; ?>
+    
+    <div class="main-content-reviews">
+        <div class="header">
+            <div class="heading">Reviews</div>
+        </div>
+        <hr>
+        <div class="search-container">
             <input type="text" 
                 class="search-bar" 
-                placeholder=" Find Reviews"
-                aria-label="Search" style="height: 20px; width : 700px">
-      </div>
-          <div class="job-card container">
-              <div class="job-card-left flex-row">
-                  <div class="pfp">
-                    <img src="<?=ROOT?>/assets/images/profile.png" alt="Profile Picture" class="profile-pic-reviewed-employee">
-                  </div>
+                placeholder="Search reviews"
+                aria-label="Search">
+            <br><br>
+            <div class="filter-container">
+                <span>Sort by:</span>
+                <select id="sortSelect" onchange="sortContent()">
+                    <option value="recent">Latest</option>
+                    <option value="views">Oldest</option>
+                </select>
+            </div>
+        </div>
+        <div class="reviews-container container">
+            
+            <div class="review-card container">
+                <div class="review-card-left flex-row">
+                    <div class="pfp">
+                        <img src="<?=ROOT?>/assets/images/person3.jpg" alt="Profile Picture" class="profile-pic-reviewed-employee">
+                    </div>
                 
-                  <div class="job-details">
-                      <h2>Noah Wick</h2>
-                      <p>Waiter</p>
-                      <p>2024/10/29 at 5.37 p.m.</p>
-                      <div style="display:flex;flex-direction:column; gap:20px">
-                        <div class="rating">
-                            <span>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                            </span>
-                        </div>
+                    <div class="review-details">
+                        <h2>Smith Greenwood</h2>
+                        <p>Bartender</p>
+                        <p>2024-11-27</p>
+                        <p>03:30 PM</p>
+                        <div style="display:flex;flex-direction:column; gap:20px">
+                            <div class="rating">
+                                <span>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                </span>
+                            </div>
                         
-                        <p class="review-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla distinctio id adipisci dicta facere tempora atque veniam! Rerum, minus expedita nobis magnam vel quibusdam natus!
-                        </p>
-                      </div>
-                  </div>
-              </div>
-              
-          </div>
+                            <p class="review-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla distinctio id adipisci dicta facere tempora atque veniam! Rerum, minus expedita nobis magnam vel quibusdam natus!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-
-
-          <div class="job-card container">
-              <div class="job-card-left flex-row">
-                  <div class="pfp">
-                    <img src="<?=ROOT?>/assets/images/person2.jpg" alt="Profile Picture" class="profile-pic-reviewed-employee">
-                  </div>
+            <div class="review-card container">
+                <div class="review-card-left flex-row">
+                    <div class="pfp">
+                        <img src="<?=ROOT?>/assets/images/person2.jpg" alt="Profile Picture" class="profile-pic-reviewed-employee">
+                    </div>
                 
-                  <div class="job-details">
-                      <h2>Sarah Williams</h2>
-                      <p>Baby-sitter</p>
-                      <p>2024/10/29 at 3.00 p.m.</p>
-                      <div style="display:flex;flex-direction:column; gap:20px">
-                        <div class="rating">
-                            <span>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                            </span>
-                        </div>
+                    <div class="review-details">
+                        <h2>Sarah Ken</h2>
+                        <p>Bartender</p>
+                        <p>2024-11-24</p>
+                        <p>03:30 PM</p>
+                        <div style="display:flex;flex-direction:column; gap:20px">
+                            <div class="rating">
+                                <span>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                </span>
+                            </div>
                         
-                        <p class="review-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla distinctio id adipisci dicta facere tempora atque veniam! Rerum, minus expedita nobis magnam vel quibusdam natus!
-                        </p>
-                      </div>
-                  </div>
-              </div>
-              
-          </div>
+                            <p class="review-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla distinctio id adipisci dicta facere tempora atque veniam! Rerum, minus expedita nobis magnam vel quibusdam natus!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-
-          <div class="job-card container">
-              <div class="job-card-left flex-row">
-                  <div class="pfp">
-                    <img src="<?=ROOT?>/assets/images/person3.jpg" alt="Profile Picture" class="profile-pic-reviewed-employee">
-                  </div>
+            <div class="review-card container">
+                <div class="review-card-left flex-row">
+                    <div class="pfp">
+                        <img src="<?=ROOT?>/assets/images/person4.jpg" alt="Profile Picture" class="profile-pic-reviewed-employee">
+                    </div>
                 
-                  <div class="job-details">
-                      <h2>Smith Greenwood</h2>
-                      <p>Bartender</p>
-                      <p>2024/10/27 at 3.30 p.m.</p>
-                      <div style="display:flex;flex-direction:column; gap:20px">
-                        <div class="rating">
-                            <span>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                                <i class="fa fa-star star-active mx-1"></i>
-                            </span>
-                        </div>
+                    <div class="review-details">
+                        <h2>Samson Waltz</h2>
+                        <p>Bartender</p>
+                        <p>2024-11-19</p>
+                        <p>05:37 PM</p>
+                        <div style="display:flex;flex-direction:column; gap:20px">
+                            <div class="rating">
+                                <span>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                    <i class="fa fa-star star-active mx-1"></i>
+                                </span>
+                            </div>
                         
-                        <p class="review-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla distinctio id adipisci dicta facere tempora atque veniam! Rerum, minus expedita nobis magnam vel quibusdam natus!
-                        </p>
-                      </div>
-                  </div>
-              </div>
-              
-          </div>
+                            <p class="review-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla distinctio id adipisci dicta facere tempora atque veniam! Rerum, minus expedita nobis magnam vel quibusdam natus!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-
-
+        </div>
+    </div>
 </div>
-
-    
-
-
-
-
-
-         
-
-    
-
-
-
