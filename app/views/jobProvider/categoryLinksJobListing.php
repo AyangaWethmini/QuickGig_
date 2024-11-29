984261770c873df57bad4d2e2376c82188b4bcf7
JaithLomitha2002 <djaithlomitha2002@gmail.com>
2024-11-29 12:16:36 +0530

    category links updated

--- app/views/jobProvider/categoryLinksJobListing.php
+++ app/views/jobProvider/categoryLinksJobListing.php
@@ -3,11 +3,11 @@
 <body>
 <script src="<?=ROOT?>/assets/js/jobProvider/jobListing.js"></script>
 <div class="category-container">
-    <a href="<?php echo ROOT;?>/jobProvider/jobListing_myJobs" class="category">My Jobs (1)</a>
-    <a href="<?php echo ROOT;?>/jobProvider/jobListing_received" class="category">Received (3)</a>
-    <a href="<?php echo ROOT;?>/jobProvider/jobListing_send" class="category">Send (1)</a>
-    <a href="<?php echo ROOT;?>/jobProvider/jobListing_toBeCompleted" class="category">To be completed (1)</a>
-    <a href="<?php echo ROOT;?>/jobProvider/jobListing_ongoing" class="category">Ongoing (1)</a>
-    <a href="<?php echo ROOT;?>/jobProvider/jobListing_completed" class="category">Completed (1)</a>
+    <a href="<?php echo ROOT;?>/jobProvider/jobListing_myJobs" class="category">My Jobs</a>
+    <a href="<?php echo ROOT;?>/jobProvider/jobListing_received" class="category">Received</a>
+    <a href="<?php echo ROOT;?>/jobProvider/jobListing_send" class="category">Send</a>
+    <a href="<?php echo ROOT;?>/jobProvider/jobListing_toBeCompleted" class="category">To be completed</a>
+    <a href="<?php echo ROOT;?>/jobProvider/jobListing_ongoing" class="category">Ongoing</a>
+    <a href="<?php echo ROOT;?>/jobProvider/jobListing_completed" class="category">Completed</a>
 </div>
 </body>
