fd4e8a77ad0e3f4ebf10b019764f1416201f8350
janusha <janushabimod@gmail.com>
2024-12-03 15:13:32 +0530

    Fix: Announcement in Home page

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,12 +1,25 @@
-<?php 
+<?php
 
 class Home extends Controller
 {
+	public function __construct()
+	{
+		$this->adminModel = $this->model('AdminModel');
+	}
+
 	protected $viewPath = "../app/views/home/";
 
 	public function index()
 	{
-		$this->view('home');
+		// Fetch announcements from the database
+		$announcements = $this->adminModel->getAnnouncements();
+
+		// Ensure the announcements key is always defined
+		$data = [
+			'announcements' => $announcements
+		];
+
+		$this->view('home', $data);
 	}
 
 	public function signup()
