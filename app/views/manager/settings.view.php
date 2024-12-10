e97fdbfe2dbdbcb745d84a6257188942b673024f
Ayanga Wethmini <wethminiayanga@gmail.com>
2024-12-10 13:39:10 +0530

    added the seperate settings page for the admin

---
+++ app/views/manager/settings.view.php
@@ -0,0 +1,12 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<?php include APPROOT . '/views/components/navbar.php'; ?>
+
+
+<div class="wrapper flex-row" style="margin-top: 100px;">
+
+    <?php require APPROOT . '/views/manager/manager_sidebar.php'; ?>
+
+</div>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?> 
