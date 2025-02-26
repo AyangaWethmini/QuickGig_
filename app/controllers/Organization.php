635af2edbb6deac512e1c614ef04e641996b3506
JaithLomitha2002 <djaithlomitha2002@gmail.com>
2025-02-26 11:11:24 +0530

    added: Implement ongoing job and request listings for organizations

--- app/controllers/Organization.php
+++ app/controllers/Organization.php
@@ -180,7 +180,14 @@ date_default_timezone_set('Asia/Colombo');
 
         function org_jobListing_ongoing(){
             $this->jobStatusUpdater->updateJobStatuses();
-            $this->view('org_jobListing_ongoing');
+            $ongoingProvider = $this->model('OngoingProvider');
+            $applyJobOngoing = $ongoingProvider->getApplyJobOngoing();
+            $reqAvailableOngoing = $ongoingProvider->getReqAvailableOngoing();
+            $data = [
+                'applyJobOngoing' => $applyJobOngoing,
+                'reqAvailableOngoing' => $reqAvailableOngoing
+            ];
+            $this->view('org_jobListing_ongoing', $data);
         }
 
         function org_jobListing_completed(){
