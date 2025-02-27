d02f112346f2b57a6fd15a8d44fbb8f81f0b97db
Ayanga Wethmini <wethminiayanga@gmail.com>
2025-02-27 22:09:04 +0530

    system reports

--- app/views/manager/report.view.php
+++ app/views/manager/report.view.php
@@ -1,11 +1,21 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<?php require_once APPROOT . '/views/inc/protectedRoute.php'; 
-protectRoute([1]);?>
-<link rel="stylesheet" href="<?=ROOT?>/assets/css/manager/plan.css"> 
+<?php require_once APPROOT . '/views/inc/protectedRoute.php';
+protectRoute([1]); ?>
+<link rel="stylesheet" href="<?= ROOT ?>/assets/css/manager/report.css">
 <?php include APPROOT . '/views/components/navbar.php'; ?>
 
 <div class="wrapper flex-row" style="margin-top: 100px;">
     <?php require APPROOT . '/views/manager/manager_sidebar.php'; ?>
-    
-    <!--  -need o add relevent settings for the admin->
+    <div class="main-content">
+        <div class="flex-container">
+            <div class="flex-item">1</div>
+            <div class="flex-item">2</div>
+            <div class="flex-item">3</div>
+            <div class="flex-item">4</div>
+        </div>
+    </div>
+
 </div>
+
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
