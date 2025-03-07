cec6e560574ef357b30366f35387abed3d95fead
Ayanga Wethmini <wethminiayanga@gmail.com>
2025-03-07 10:16:13 +0530

    Only once

--- app/views/home/home.view.php
+++ app/views/home/home.view.php
@@ -3,7 +3,11 @@
 <?php include APPROOT . '/views/components/navbar.php'; ?>
 
 <?php
-if (!isset($_SESSION['subscription_popup_shown'])) {
+if (
+    !isset($_SESSION['subscription_popup_shown']) &&
+    isset($_SESSION['user_role']) &&
+    $_SESSION['user_role'] > 1
+) {
     include APPROOT . '/views/components/subscriptions.php';
     $_SESSION['subscription_popup_shown'] = true;
 }
